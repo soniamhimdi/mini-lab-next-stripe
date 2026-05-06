3d9cd911ea4e06763b22818863371ecdae0c863c
soniamhimdi <e2595653@cmaisonneuve.qc.ca>
2026-05-06 08:27:58 -0400

    first commit -project structure

---
+++ lib/prisma.ts
@@ -0,0 +1,18 @@
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from "../app/generated/prisma/client";
+
+import dotenv from "dotenv";
+dotenv.config();
+ 
+// éCrer le client Neon adapter
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL,
+});
+ 
+// éCrer le client Prisma avec l’adaptateur Neon
+const prisma = new PrismaClient({
+  adapter,
+  log: ['query', 'info', 'warn', 'error'],
+});
+
+export default prisma
