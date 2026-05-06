81a69ee41ef6da3c24e18c1a0765e2c2661221a1
soniamhimdi <e2595653@cmaisonneuve.qc.ca>
2026-05-06 15:05:47 -0400

    modif-prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaNeon } from '@prisma/adapter-neon';
-import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client";
 
 import dotenv from "dotenv";
 dotenv.config();
